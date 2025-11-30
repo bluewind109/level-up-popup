1ab45fa93483a62cac6d0690f1fd9ae0fb9050e1
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2025-12-01 01:28:25 +0700

    Update level up panel: Hide panel on clicking OK button

--- scenes/level_up_panel.gd
+++ scenes/level_up_panel.gd
@@ -3,14 +3,22 @@ class_name PanelLevelUp
 
 @export var card_prefab = preload("res://scenes/level_up_card.tscn")
 @export var card_container: HBoxContainer
+@export var button_ok: Button
 
 var tween_duration: float = 0.5
+var is_animation_done: bool = false
 
 func _ready() -> void:
+	if (button_ok):
+		button_ok.pressed.connect(_on_button_ok)
 	show_popup()
 
 func show_popup() -> void:
 	print("show_popup")
+	is_animation_done = false
+	for i in card_container.get_children():
+		card_container.remove_child(i)
+		i.queue_free()
 	_tween_show_panel()
 	await get_tree().create_timer(tween_duration).timeout
 	_tween_show_card()
@@ -23,7 +31,7 @@ func _tween_show_panel() -> void:
 	tween.set_ease(Tween.EASE_OUT)
 	tween.set_parallel(true)
 	tween.tween_property(self.get_parent(), "position:y", 0, 0.125)
-	tween.tween_property(self.get_parent(), "modulate:a", 1.0, 0.125)
+	tween.tween_property(self.get_parent(), "modulate:a", 1, 0.125)
 
 func _tween_show_card() -> void:
 	if (not card_prefab): return
@@ -32,8 +40,8 @@ func _tween_show_card() -> void:
 		var card_instance = card_prefab.instantiate() as CardLevelUp
 		card_container.add_child.call_deferred(card_instance)
 		await card_instance.ready
-		card_arr[i].enable_selection(false)
 		card_arr.append(card_instance)
+		card_instance.enable_selection(false)
 	
 	for i in card_arr.size():
 		card_arr[i].show_card.call_deferred()
@@ -41,3 +49,17 @@ func _tween_show_card() -> void:
 
 	for i in card_arr.size():
 		card_arr[i].enable_selection(true)
+	is_animation_done = true
+
+func _tween_hide_panel():
+	self.get_parent().modulate.a = 1
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(self.get_parent(), "modulate:a", 0, 0.125)
+
+func _on_button_ok() -> void:
+	print("_on_button_ok")
+	if (not is_animation_done): return
+	_tween_hide_panel()
+	# TODO Whatever logic you want to put in
